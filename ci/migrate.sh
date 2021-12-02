37351a470c4678cf91d4cb109ab1150e9d2fb87f
Reginald Ross <reggieross@reggies-mbp.lan>
2021-12-01 19:18:05 -0600

    rross | updating migrtion script

--- ci/migrate.sh
+++ ci/migrate.sh
@@ -4,18 +4,21 @@ set -e
 APP_LIST=("catalog-gql");
 
 runCatalogGQLMigation () {
-  npm i
-#  if [ -z ${DB_USERNAME+x} ]; then export DB_USERNAME=integration-test-user; else echo "DB_USERNAME is set"; fi
-#  if [ -z ${DB_PASSWORD+x} ]; then export DB_PASSWORD=passw0rd; else echo "DB_PASSWORD is set"; fi
-#  if [ -z ${DB_HOST+x} ]; then export DB_HOST=localhost; else echo "DB_HOST is set"; fi
-#  if [ -z ${DB_NAME+x} ]; then export DB_NAME=MarketplaceCatalog; else echo "DB_NAME is set"; fi
-  npm run migrate
+  if [ -z ${DB_USERNAME+x} ]; then export DB_USERNAME=integration-test-user; else echo "DB_USERNAME is set"; fi
+  if [ -z ${DB_PASSWORD+x} ]; then export DB_PASSWORD=passw0rd; else echo "DB_PASSWORD is set"; fi
+  if [ -z ${DB_HOST+x} ]; then export DB_HOST=localhost; else echo "DB_HOST is set"; fi
+  if [ -z ${DB_NAME+x} ]; then export DB_NAME=MarketplaceCatalog; else echo "DB_NAME is set"; fi
+  npx knex migrate:latest --knexfile ./catalog-gql/knexfile.js
+}
+
+newAppMigation () {
+  echo "Running migration for new App"
 }
 
 runMigrationForApp () {
   APP=$1
   case $APP in "catalog-gql") runCatalogGQLMigation;;
-  "new-app") DB_NAME="another-db";;
+  "new-app") newAppMigation;;
   *) echo "public";;
   esac
 }
@@ -23,9 +26,7 @@ runMigrationForApp () {
 
 for APP in "${APP_LIST[@]}"
 do
-  APP_PATH="./$APP"
-  cd "$APP_PATH" || exit
-  runCatalogGQLMigation "$APP"
+  runMigrationForApp "$APP"
 done
 
 exit 0
